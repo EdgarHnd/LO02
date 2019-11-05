da23321bfa14b8fec37b300a9e272b53950ac1fc
Edgar Haond <edgarhnd@wifi-etudiants2351.utt.fr>
2019-11-05 11:24:39 +0100

    Add menu

--- src/JestGame/GameManager.java
+++ src/JestGame/GameManager.java
@@ -1,10 +1,52 @@
 package JestGame;
+import java.util.Scanner;
 
 public class GameManager {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Main menu
+		System.out.println("Welcome to JestGame !");
+		
+		
+		int userChoice;
+
+        userChoice = menu();
+        
+        switch (userChoice) {
+        case 1:
+        	System.out.println("Game starting");
+        	break;
+        case 2:
+        	System.out.println("Opening settings");
+        	break;
+        case 3:
+        	System.out.println("Quiting..");
+        	System.exit(0);
+        	break;
+        
+        }
 
 	}
+	
+		//Menu selection method
+	
+	public static int menu() {
 
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        
+
+        System.out.println("Choose from these choices");
+        System.out.println("-------------------------\n");
+        System.out.println("1 - Start game");
+        System.out.println("2 - Setup game");
+        System.out.println("3 - Quit");
+        
+
+        selection = input.nextInt();
+        input.close();
+        return selection;    
+    }
 }
